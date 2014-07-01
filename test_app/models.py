38acbef79ebe5faabe76346ed645a576f5878ad9
user <kulesh.serega@gmail.com>
2014-07-01 15:55:39 +0300

    new1

--- test_app/models.py
+++ test_app/models.py
@@ -7,4 +7,4 @@ class Poll(models.Model):
 class Choice(models.Model):
 	poll = models.ForeignKey(Poll)
 	choice_text = models.CharField(max_length=200)
-	votes = models.IntegerField(default=0)
+	votes = models.IntegerField(default=0) 
